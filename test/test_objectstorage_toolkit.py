3ad1cb2d92483b384a2258595ae1b217a211a389
markheger <mark-oliver.heger@de.ibm.com>
2019-08-29 08:25:52 +0000

    update test script for CP4D

--- test/test_objectstorage_toolkit.py
+++ test/test_objectstorage_toolkit.py
@@ -529,10 +529,10 @@ class TestICP(TestDistributed):
         super().setUpClass()
         env_chk = True
         try:
-            print("STREAMS_REST_URL="+str(os.environ['STREAMS_REST_URL']))
+            print("CP4D_URL="+str(os.environ['CP4D_URL']))
         except KeyError:
             env_chk = False
-        assert env_chk, "STREAMS_REST_URL environment variable must be set"
+        assert env_chk, "CP4D_URL environment variable must be set"
 
 class TestICPLocal(TestICP):
     """ Test in ICP env using local installed toolkit (STREAMS_INSTALL/toolkits) """
